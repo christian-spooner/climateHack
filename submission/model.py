a3a28f67569ffc72c3cc5261c8bcb8b53e60e1dc
Christian Spooner <cswspooner@gmail.com>
2022-03-16 13:54:41 +0000

    final model

--- submission/model.py
+++ submission/model.py
@@ -1,23 +1,40 @@
 import torch
 import torch.nn as nn
 
-#########################################
-#       Improve this basic model!       #
-#########################################
-
 
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.layer1 = nn.Linear(in_features=12 * 128 * 128, out_features=256)
-        self.layer2 = nn.Linear(in_features=256, out_features=256)
-        self.layer3 = nn.Linear(in_features=256, out_features=24 * 64 * 64)
+        self.down_pool = nn.MaxPool3d(kernel_size=(1, 2, 2))
+        self.layer1 = nn.Conv3d(1, 4, kernel_size=5, stride=(1, 2, 2), padding=1)
+        self.layer2 = nn.Conv3d(4, 16, kernel_size=3, stride=(1, 2, 2), padding=1)
+        self.layer3 = nn.Conv3d(16, 16, kernel_size=3, stride=(1, 2, 2), padding=0)
+        self.pool = nn.MaxPool3d(kernel_size=(1, 2, 2))
+        self.flatten = nn.Flatten()
+        self.linear = nn.Linear(128, 24 * 64 * 64)
 
     def forward(self, features):
-        x = features.view(-1, 12 * 128 * 128) / 1024.0
-        x = torch.relu(self.layer1(x))
-        x = torch.relu(self.layer2(x))
-        x = torch.relu(self.layer3(x))
 
-        return x.view(-1, 24, 64, 64) * 1024.0
+        x = features.unsqueeze(dim=1) / 1023.0
+        x = torch.relu(self.layer1(x))
+        x = torch.relu(self.pool(x))
+        x = torch.relu(self.layer2(x))
+        x = torch.relu(self.pool(x))
+        x = torch.relu(self.layer3(x))
+        x = torch.relu(self.pool(x))
+        x = self.flatten(x)
+        x = torch.relu(self.linear(x))
+        x = x.view(-1, 24, 64, 64) * 1023.0
+
+        a = features.shape[0]
+        x2 = features[0:a, 11, 32:96, 32:96]
+        x2 = torch.unsqueeze(x2, dim=1)
+        y2 = []
+        for i in range(24):
+            y2.append(x2)
+        x2 = torch.cat(y2, dim=1)
+
+        z = (0.5 * x) + (0.5 * x2)
+
+        return z
